dcc983b827c94268cad93da2dbd94c0188607935
FredyVia <fredyvia@qq.com>
2022-11-29 14:30:41 +0800

    2022-11-29 | 1. init

--- src/lesson01/src/kernel.c
+++ src/lesson01/src/kernel.c
@@ -1,11 +1,31 @@
 #include "mini_uart.h"
-
-void kernel_main(void)
+static unsigned int curr_p = 0;
+void hello(unsigned int nu)
 {
-	uart_init();
-	uart_send_string("Hello, world!\r\n");
+	char str[2];
+	if (nu == 0)
+	{
+		uart_init();
+	}
+	else
+	{
+		while (curr_p != nu)
+			;
+	}
+	str[0] = '0' + nu;
+	str[1] = '\0';
+	uart_send_string("Hello, world from ");
+	uart_send_string(str);
+	uart_send_string("!!!!\r\n");
+	curr_p++;
+	while (curr_p != 4)
+		;
+}
 
-	while (1) {
+void ping_pong()
+{
+	while (1)
+	{
 		uart_send(uart_recv());
 	}
 }
